c3e8fe25c03aeb7b81746b169c68e9102070ad13
onedayxzn <sukmaramadhanasri@gmail.com>
2021-06-03 23:59:48 +0700

    edit

---
+++ src/components/Hero.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="Hero">
+    <!-- dekstop -->
+    <div class="d-none d-md-block">
+      <div class="row mt-2">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo Segera Pilih dan Pesan Makanan Favorit Anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" alt="" width="100%" />
+        </div>
+      </div>
+    </div>
+
+    
+
+    <!-- mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <img src="../assets/images/hero.png" alt="" width="100%" />
+        </div>
+        <div class="col-md-6 mb-3">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2>
+                <strong>Delicious Food Menu,</strong> <br />
+                in Your Gadget
+              </h2>
+              <p>Ayo Segera Pilih dan Pesan Makanan Favorit Anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right></b-icon-arrow-right> Pesan
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style></style>
